00104f6ec13c6f1ccb03306df791f5691d4fd96b
Tanner Kirkpatrick <twk4491@gmail.com>
2020-10-22 13:54:18 -0400

    started building out inquirer prompts and team functions

--- app.js
+++ app.js
@@ -7,7 +7,82 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
  
-const render = require("./lib/htmlRenderer"); 
+const render = require("./lib/htmlRenderer");
+
+const teamArray = [];
+
+
+init();
+
+function init(){
+    console.log("Welcome to the Dev Team Generator!");
+    console.log("This application will help you create an HTML webpage containing information about your development team")
+    console.log("Answer the following questions to begin building your team profile");
+    console.log("--------------------------------------------------------------------");
+
+    initManager();
+
+    function initManager(){
+
+        console.log("First, let's gather some information about your manager")
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your manager's name?",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "What is your managers ID #?",
+                name:"managerId"
+            },
+            {
+                type: "input",
+                message: "What is your manager's email?",
+                name: "managerEmail"
+            },
+            {
+                type: "input",
+                message: "What is your manager's office number?",
+                name: "managerOfficeNum"
+            }
+    
+        ]).then(answers => {
+            const managerObj = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
+            teamArray.push(managerObj);
+            buildTeam();
+        })
+    }
+
+    function buildTeam(){
+        console.log("Okay!  Now that we have information for your manager, let's add the rest of the team");
+
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Would you like to add an Engineer or an Intern?",
+                choices: ["Engineer", "Intern", "I don't want to add any additional team members"],
+                name: "positionChoice"
+            }
+        ]).then(userPick => {
+            if (userPick.positionChoice === "Engineer"){
+                //run engineer funciton here
+            } else if (userPick.positionChoice === "Intern"){
+                //run intern function here
+            } else{
+                //write html function here
+            }
+        })
+    }
+
+    function initEngineer(){
+        console.log("Cool, let's add some information for your Software Engineer!")
+
+        //remember to run buildteam function at end of function so the user is presented with the option of adding another team member
+    }
+}
+
+
 
 
 
